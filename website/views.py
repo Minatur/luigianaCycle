cf4835f4811634b45de2123e6fe5744a0a8fcbab
Minatur <68608254+Minatur@users.noreply.github.com>
2022-07-25 21:20:16 +0200

    template working; webpage loads list of rides
    note: gitignore doesn't work

--- website/views.py
+++ website/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.template import loader
 
 from .models import ride
 
 
 def index(request):
-    return HttpResponse("Hello world! You are at the website for Luigiana.")
+    rides_list = ride.objects.all()
+    template = loader.get_template('index.html')
+    context = {
+        'rides_list': rides_list,
+    }
+    return HttpResponse(template.render(context, request))
+    #return HttpResponse("Hello world! You are at the website for Luigiana.")
+
+
+
 # Create your views here.
 class AboutView(TemplateView):
-    template_name = "about.html"
+    template_name = "website_base.html"
